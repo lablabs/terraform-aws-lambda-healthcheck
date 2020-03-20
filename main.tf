288aab3269555563ad4d9cd34926abc72cfddff3
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2020-03-20 08:55:15 +0100

    Change region for alarm

--- main.tf
+++ main.tf
@@ -122,6 +122,7 @@ resource "aws_lambda_permission" "this" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "default" {
+  provider                  = aws.alarm
   alarm_name                = "${var.target_url} not healthy"
   comparison_operator       = "GreaterThanThreshold"
   evaluation_periods        = "2"
